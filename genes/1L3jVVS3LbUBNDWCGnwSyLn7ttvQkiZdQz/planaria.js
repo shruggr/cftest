e4c1406076fa9fbbabd8d95ba0a532129bebfd19
David Case <david.case@shruggr.cloud>
2019-02-23 19:21:00 -0500

    Index battle

--- genes/1L3jVVS3LbUBNDWCGnwSyLn7ttvQkiZdQz/planaria.js
+++ genes/1L3jVVS3LbUBNDWCGnwSyLn7ttvQkiZdQz/planaria.js
@@ -3,19 +3,69 @@
 * API Reference: https://docs.planaria.network/#/api?id=anatomy-of-a-planaria
 *
 ***************************************/
+const BATTLE = "18pojMVnZYDa19aqEo46ZdE1FSQBwU54zX";
+const COMMIT = "1DknJFhdEkwgn4hCWL4iCBWg7ZKm1h2hNC";
+const INIT = "1AS2ESCbWKo6HVbEy7aznkCgQunPfT6hBN";
+const STATE = "1KgH6URXsAxte9xsuetviVd2kH4Zn3YU1C";
+const VALIDATOR = "149qjwS8MsJQszQu7Rsybya2zrm6Ff5Qhe";
+const FIGHTER = "167WW15VDSqTx4EJ8RtUtVUWxhSy6HZ6kk";
+
 module.exports = {
   planaria: '0.0.1',
-  from: HEIGHT,
+  from: 570000,
   name: 'cftest',
   version: '0.0.1',
   description: '',
   address: '1L3jVVS3LbUBNDWCGnwSyLn7ttvQkiZdQz',
-  index: {},
+  index: {
+    battle: {
+      keys: ['txnId'],
+      unique: ['txnId']
+    }
+  },
   onmempool: async function(m) {
-    // Triggered for every mempool tx event
-    // https://docs.planaria.network/#/api?id=onmempool
+    // const opRet = m.input.out.find((out) => out.b0.op == 106);
+    // if(opRet && opRet.s1 == BATTLE) {
+    //   m.state.create({
+    //     name: 'battle',
+    //     data: {
+    //       txnId: m.index.tx.h,
+    //       hashChain: opRet.s2,
+    //       commitTxns: [opRet.s3, opRet.s4]
+    //     }
+    //   })
+    // }
   },
   onblock: async function(m) {
+    const txns = m.input.items.filter((txn) => {
+      const opRet = txn.out.find((out) => out.b0.op == 106);
+      return opRet && opRet.s1 == BATTLE;
+    });
+
+    await Promise.all(txns.map((txn) => {
+      const opRet = txn.out.find((out) => out.b0.op == 106);
+      m.data
+    }))
+
+    if(opRet && opRet.s1 == BATTLE) {
+      m.state.create({
+        name: 'battle',
+        data: m.input.items
+          .filter((txn) => {
+            const opRet = txn.out.find((out) => out.b0.op == 106);
+            return opRet && opRet.s1 == BATTLE;
+          })
+          .map((txn) => {
+            const opRet = txn.out.find((out) => out.b0.op == 106);
+            return {
+              txnId: txn.tx.h,
+              hashChain: opRet.s2,
+              commitTxns: [opRet.s3, opRet.s4]
+            }
+          }),
+        onerror: (e) => { if (e.code != 11000) { process.exit() } }
+      })
+    }
     // Triggered for every new block event
     // https://docs.planaria.network/#/api?id=onblock
   },
